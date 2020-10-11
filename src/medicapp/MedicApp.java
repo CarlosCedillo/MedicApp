dbb61cf65b62dfdf8382f32c73afada48c7220d6
CarlosCedillo <cgca_0223@hotmail.com>
2020-10-10 19:12:22 -0600

    First commit: Project created

---
+++ src/medicapp/MedicApp.java
@@ -0,0 +1,19 @@
+/*
+This is the class who start the app
+ */
+package medicapp;
+
+/**
+ *
+ * @author Cageceal
+ */
+public class MedicApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
