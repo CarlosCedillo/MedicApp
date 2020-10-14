1c4480f63b8358135ed3d992ec9e87a7c5a190b1
CarlosCedillo <cgca_0223@hotmail.com>
2020-10-13 22:19:49 -0600

    Imeges added, library for sending email, login created

--- src/medicapp/MedicApp.java
+++ src/medicapp/MedicApp.java
@@ -3,11 +3,9 @@
 
 package medicapp;
 
-import com.sun.xml.internal.stream.Entity;
 import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
-import java.util.Scanner;
-import views.Index;
+import views.Login;
 
 /**
  * @author Cageceal
@@ -21,50 +19,8 @@ public class MedicApp {
      */
     public static void main(String[] args) throws SQLException, NoSuchAlgorithmException {
         // TODO code application logic here
-        
-        //System.out.println("--- Executing MedicApp: created by Carlos Gerardo Cedillo Alcántar ---");
-        
-//        pruebas();
-        
-//        Index index = new Index();
-//        index.setVisible(true);
-        
-//        DataBase dataBase = new DataBase();
-//        dataBase.createDB();
-        
-    }
-
-    private static void pruebas() {
-        Helper helper = new Helper();
-        
-        System.out.println("Creando palabra de seguridad...");
-        String secureWord = helper.creteSecurityWord();
-        System.out.println("\nLa palabra creada es: "+secureWord);
-        Integer[] tI = new Integer[3];
-        tI = helper.getTime();
-        System.out.println("Tiempo de inicio: "+tI[0]+":"+tI[1]+":"+tI[2]);
-        
-        Scanner keyBoard = new Scanner(System.in);
-        System.out.println("\nAhora quiero que ingrese la palbra de arriba: ");
-        String toCompare = keyBoard.next();
-        
-        Integer[] tF = new Integer[3];
-        tF = helper.getTime();
-        System.out.println("\nTiempo de finalizacion: "+tF[0]+":"+tF[1]+":"+tF[2]);
-        
-        Integer timeSeconds;
-        timeSeconds = Helper.restTime(tI,tF);
-        System.out.println("Tiempo trsancurrido = "+timeSeconds+" Segundos");
-        
-        //limite de 5 segundos
-        Boolean validation = Helper.validation(timeSeconds, 5);
-        
-        if( validation == true ){
-            System.out.println("To' bien :)");
-        }else{
-            System.out.println("To' mal :(");
-        }
-        
+        Login login = new Login();
+        login.setVisible(true);
     }
 
 }
